ed72b638d7924f83a0d82ee07f6d024ecf0861f7
alsco1234 <80326391+alsco1234@users.noreply.github.com>
2023-01-20 23:42:27 +0900

    [CREATE] reshape npy

---
+++ feature_reshape.py
@@ -0,0 +1,11 @@
+import os
+import numpy as np
+
+path = '/home/s21900395/video_features/crash_32_18_r21d/r21d/'
+
+for i in os.listdir(path):
+    data = np.load(path+i)
+    reshape_data = data.reshape(1,-1)
+    np.save('/home/s21900395/video_features/r21d_crash_reshape/'+i, reshape_data)
+    print('file ', i, ' saved')
+
