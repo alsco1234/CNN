bdb47e649b26a9258e970c9c528f6a310ca83fe4
alsco1234 <80326391+alsco1234@users.noreply.github.com>
2023-01-31 22:55:35 +0900

    [UPDATE] video_2_subvideos

--- video_2_subvideos.py
+++ video_2_subvideos.py
@@ -5,11 +5,18 @@ success,image = vidcap.read()
 count = 1
 success = True
 
+subnum = 0
+
 while success:
   success,image = vidcap.read()
-  cv2.imwrite("../../extraresults/sun1_%d.jpg" % count, image)
-  print("saved image %d.jpg" % count)
+  cv2.imwrite("../../extraresults/%d/sun1_%d.jpg" % subnum % count, image)
+  print("saved image %d.jpg in %d folder." % count % subnum)
   
-  if cv2.waitKey(10) == 16:                    
+  if cv2.waitKey(10) == 27:                    
       break
   count += 1
+
+  if count%16 == 0:
+     subnum +=1
+     
+     
