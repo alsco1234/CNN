00e9bcf42be2d6c98265390acfe2e0e41a07dafd
alsco1234 <80326391+alsco1234@users.noreply.github.com>
2023-02-03 22:33:45 +0900

    [CREATE] pick_frames.py

---
+++ pick_frames.py
@@ -0,0 +1,12 @@
+import os
+ 
+folder_dir = 'projects/vode/DEC_origins'
+folder_list = os.listdir(folder_dir)
+
+for foldername in folder_list:
+    path_dir = folder_dir + "/" + foldername
+    file_list = os.listdir(path_dir)
+
+    for filename in file_list:
+        if (int(filename[0:4]) + 3) % 3 !=0:
+            os.remove(path_dir + "/" + filename)
