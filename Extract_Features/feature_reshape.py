97d9b3b1cf03ac720a3e6253d22d6e5772496136
alsco1234 <80326391+alsco1234@users.noreply.github.com>
2023-02-24 23:57:16 +0900

    [UPDATE]

--- Extract_Features/feature_reshape.py
+++ Extract_Features/feature_reshape.py
@@ -8,6 +8,6 @@ path = '/home/s21900395/video_features/crash_32_18_r21d/r21d/'
 for i in os.listdir(path):
     data = np.load(path+i)
     reshape_data = data.reshape(1,-1)
-    np.save('/home/s21900395/video_features/r21d_crash_reshape/'+i, reshape_data)
+    np.save('img_reshape'+i, reshape_data)
     print('file ', i, ' saved')
 
