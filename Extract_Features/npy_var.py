307c252be6b721ce76623f7209985c63c0d6133a
alsco1234 <80326391+alsco1234@users.noreply.github.com>
2023-02-28 23:20:22 +0900

    [UPDATE] upate

--- Extract_Features/npy_var.py
+++ Extract_Features/npy_var.py
@@ -3,15 +3,15 @@
 import numpy as np
 from PIL import Image
 
-f = '../s3d_0217ver_test.npy'
+f = '../../27.npy'
 tmp = np.load(f, allow_pickle=True)
 
-#tmp = tmp.tolist()
-# for j in tmp:
-#     print(i,': ',j)
+tmp = tmp.tolist()
+#for j in tmp:
+#    print(i,': ',j)
 #tmp['s3d'][0]로 뽑아
 
 # if npz, instead of npy : using tmp['arr_1']
 
-print(tmp.shape)
+#print(tmp.shape)
 print(tmp)
