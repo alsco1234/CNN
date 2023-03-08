485e1389dd2ad1473e1ecb9486f8dca607526ee1
alsco1234 <80326391+alsco1234@users.noreply.github.com>
2023-03-08 20:25:29 +0900

    [CREATE] segment_kinetics.py

---
+++ Proj2_Video_Recognition_using_CNN/segment_kinetics.py
@@ -0,0 +1,46 @@
+import cv2
+import os
+from os import path
+from pathlib import Path
+
+# filepath = '../../../projects/vode/CCD/Normal/000004.mp4'
+filepath = '../../../projects/vode/data/kinetics400/test/'
+# vidcap = cv2.VideoCapture(filepath)
+
+file_names = os.listdir('../../../projects/vode/data/kinetics400/test/')
+file_list = list(file_names)
+
+for file in file_list:
+    video_names = os.listdir('../../../projects/vode/data/kinetics400/test/' + file + '/')
+    video_list = list(video_names)
+    for video in video_list:
+        count = 0
+        file_num = 0
+        str = video.split('.')
+        vidcap = cv2.VideoCapture(filepath + file + '/' + video)
+        print(filepath + file + '/' + video)
+        while(vidcap.isOpend()):
+
+            ret, image = vidcap.read()
+
+            if count % 2 == 0:
+                dir_path = Path("../../../projects/vode/data/video_segment/kinetics400_100/test/" + file)
+                if dir_path.is_dir() == True:
+                    video_path = Path("../../../projects/vode/data/video_segment/kinetics400_100/test/" + file + '/' + str[0])
+                    if video_path.is_dir() == True:
+                        cv2.imwrite("../../../projects/vode/data/video_segment/kinetics400_100/test/" + file + '/' + str[0] + "/" + "%d.jpg" %file_num, image)
+                        file_num += 1
+                    else:
+                        os.makedirs("../../../projects/vode/data/video_segment/kinetics400_100/test/" + file + '/' + str[0])
+                        cv2.imwrite("../../../projects/vode/data/video_segment/kinetics400_100/test/" + file + '/' + str[0] + "/" + "%d.jpg" %file_num, image)
+                        file_num += 1
+                else:
+                    os.makedirs("../../../projects/vode/data/video_segment/kinetics400_100/test/" + file)
+                    os.makedirs("../../../projects/vode/data/video_segment/kinetics400_100/test/" + file + '/' + str[0])
+                    cv2.imwrite("../../../projects/vode/data/video_segment/kinetics400_100/test/" + file + '/' + str[0] + "/" + "%d.jpg" %file_num, image)
+                    file_num += 1
+            count += 1
+
+            if file_num == 100:
+                print("Saved " + file + "Done")
+                break
