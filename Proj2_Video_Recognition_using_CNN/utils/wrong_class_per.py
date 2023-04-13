ca358be1e7dcdd19d43fed3586f734a1ee83eb89
alsco1234 <80326391+alsco1234@users.noreply.github.com>
2023-04-13 23:57:22 +0900

    [CREATE] wrong class per

---
+++ Proj2_Video_Recognition_using_CNN/utils/wrong_class_per.py
@@ -0,0 +1,45 @@
+import os
+import csv
+
+predic_class = dict()
+answer_class = dict()
+pred_total = 0
+ans_total = 0
+
+f = open('./s3d_wrong_prediction.csv')
+path_reader = csv.reader(f)
+
+for line in path_reader:
+    now_class = line[0].split('/')[-2]
+    if predic_class.get(now_class) == None:
+        predic_class[now_class] = 1
+        pred_total += 1
+    else:
+        predic_class[now_class] += 1
+        pred_total += 1
+f.close()
+# for key, val in predic_class.items():
+#     print(key, val)
+
+# print("## TOTAL IS", total)
+
+f = open("./i3d+r21d+x3d_path.txt")
+txtlines = f.readlines()
+
+for line in txtlines:
+    now_class = line.split('/')[-2]
+    if answer_class.get(now_class) == None:
+        answer_class[now_class] = 1
+        ans_total += 1
+    else:
+        answer_class[now_class] += 1
+        ans_total += 1
+f.close()
+
+percent_class = dict()
+for key, val in answer_class.items():
+    if predic_class.get(key) == None:
+        percent_class[key] = 1.0
+    else:
+        percent_class[key] = round((val - predic_class[key]) / val,3)
+        print(key, " : ", percent_class[key])
